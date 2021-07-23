2cc77d7fff730f8bb96dd2f668a9bcfb796aede2
victorsnunes <victorsnunes99@gmail.com>
2021-07-23 21:41:52 +0100

    Setting Attributes

--- 2 - Counter App/counter-app/src/components/counter.jsx
+++ 2 - Counter App/counter-app/src/components/counter.jsx
@@ -6,11 +6,16 @@ class Counter extends Component {
         count: 0
     }
 
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
+
     render() {
         return (
             <React.Fragment>
-                <span>{this.formatCount}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
