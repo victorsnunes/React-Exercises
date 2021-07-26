ac4a374c5ca17aa3d22257e95788eb3f3cff2bba
victorsnunes <victorsnunes99@gmail.com>
2021-07-26 16:05:52 +0100

    Passing Data to Components

--- 2 - Counter App/counter-app/src/components/counter.jsx
+++ 2 - Counter App/counter-app/src/components/counter.jsx
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0
+        value: this.props.value
     }
 
     handleIncrement = (product) => {
         console.log(product)
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     }
 
     render() {
+        console.log('props', this.props);
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -22,12 +23,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "btn m-2 btn-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
--- 2 - Counter App/counter-app/src/components/counters.jsx
+++ 2 - Counter App/counter-app/src/components/counters.jsx
@@ -13,7 +13,8 @@ class Counters extends Component {
     render() {
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} />)}
+                {this.state.counters.map(counter =>
+                    <Counter key={counter.id} value={counter.value} />)}
             </div>);
     }
 }
