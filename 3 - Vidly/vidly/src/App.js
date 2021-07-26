f2f6a224f2c1decec920cf27970050305dbb9390
victorsnunes <victorsnunes99@gmail.com>
2021-07-26 14:54:08 +0100

    Deleting a Movie

---
+++ 3 - Vidly/vidly/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Movies from './components/movies';
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
